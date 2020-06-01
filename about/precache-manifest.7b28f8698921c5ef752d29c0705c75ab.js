fdc06360e05a40a806a3b2dca1904c07e8da4074
Raphael Marques <jose.raphael.marques@gmail.com>
2020-05-31 22:05:15 -0300

    site about

---
+++ about/precache-manifest.7b28f8698921c5ef752d29c0705c75ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "678bbd931f2ce973be20",
+    "url": "/css/app.1a1dcc71.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "412b8e53657ba9b0cf8e344d68d4d22b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3aba96b8e08268f4ba2d",
+    "url": "/js/about.4e2054a7.js"
+  },
+  {
+    "revision": "678bbd931f2ce973be20",
+    "url": "/js/app.617f618a.js"
+  },
+  {
+    "revision": "015d5012664ee9b34793",
+    "url": "/js/chunk-vendors.ab5c18c0.js"
+  },
+  {
+    "revision": "ceb3f2341ad03819945a32ad18ee8c37",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
